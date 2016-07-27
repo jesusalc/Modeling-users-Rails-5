1e267dcc9ef1b7b69d4f8ad595c485244da7ea3c
jesusalc <jesusalc@gmail.com>
2016-07-27 13:16:42 +0200

    passed format validation test by adding regex to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
+  # https://www.railstutorial.org/book/modeling_users
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+
+  # See http://www.rubular.com/ for regular expressions
+  # Expression	  Meaning
+  # /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i	full regex
+  # /	            start of regex
+  # \A	          match start of a string
+  # [\w+\-.]+	    at least one word character, plus, hyphen, or dot
+  # @	            literal “at sign”
+  # [a-z\d\-.]+	  at least one letter, digit, hyphen, or dot
+  # \.	          literal dot
+  # [a-z]+	      at least one letter
+  # \z	          match end of a string
+  # /	            end of regex
+  # i	            case-insensitive
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
 end
